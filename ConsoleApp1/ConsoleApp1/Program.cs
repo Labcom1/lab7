bee7498977771eed4d10a58a16c2a762bdccd61b
Sirada Soonthorn <ploy_puth@outlook.co.th>
2019-11-15 14:18:35 +0700

    yt

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   class Teacher
+{
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("Tom", 350f);
+            // teacher work for 20Hr/month
+            Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
+                teacher.CalculateCharge(20f));
+            Console.ReadLine();
+        }
+        // figure out the charge based on teacher's rate
+        public float CalculateCharge(float hours)
+    {
+        return (hours * billingRate);
+    }
+    // return the name of this type
+    public string TypeName()
+    {
+        return ("Teacher");
+    }
+    private string name;
+    protected float billingRate;
+}
+
+
+}
+    
+
